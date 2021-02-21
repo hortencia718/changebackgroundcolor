82028cf9c02f50a8a83c5776f1548ebb5e7e2dba
hortencia718 <hortenciaecisneros@gmail.com>
2021-02-21 12:54:36 -0500

    background

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// my array of colors
+let colors =['lime', 'lightyellow', 'orange','lightpink','brown','fuchsia', 'red', 'black','white','lightblue','turquoise','magenta'];
+// get button 
+let button = document.getElementById('button');
+// add eventlistener 
+button.addEventListener('click', function(){
+    // selector of color
+    let randomColor = colors[Math.floor(Math.random() * colors.length)]
+    // get container
+    let container = document.getElementById('container');
+    // container element gets random color from array of colors
+    container.style.background = randomColor ;
+})
+
+
+
+
+
+
+
